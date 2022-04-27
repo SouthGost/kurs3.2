b005c7c8c20369f1d5b24687a788a5a30f740fc4
An_Pavlov <dgaoling31@gmail.com>
2022-04-27 13:46:36 +0300

    .

--- public/script.js
+++ public/script.js
@@ -1,11 +1,8 @@
-// import * as THREE from 'three'
-// import { OrbitControls } from './jsm/controls/OrbitControls.js'
-// import Stats from './jsm/libs/stats.module.js'
-// import { GUI } from './jsm/libs/lil-gui.module.min.js'
 import * as THREE from 'three'
-import { OrbitControls } from './jsm/controls/OrbitControls.js'
-import Stats from './jsm/libs/stats.module.js'
 import { PointerLockControls } from './jsm/controls/PointerLockControls.js';
+import {GLTFLoader} from './jsm/loaders/GLTFLoader.js';
+import { clone } from './jsm/utils/SkeletonUtils.js';
+
 
 
 const canvas = document.getElementById('canvas');
@@ -18,7 +15,6 @@ canvas.setAttribute('height', height);
 const renderer = new THREE.WebGLRenderer({ canvas: canvas });
 renderer.setClearColor(0x000000);
 
-
 const scene = new THREE.Scene();
 
 let camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 5000)
@@ -26,6 +22,48 @@ camera.position.set(0, 0, 1000);
 
 const light = new THREE.AmbientLight(0xffffff);
 scene.add(light);
+
+
+
+
+const startButton = document.getElementById("menu_start_button");
+const menuLoading = document.getElementById("menu_loading");
+const menu = document.getElementById("menu");
+// const models = {
+//     flamingo: {
+//         url: '/models/flamingo.glb'
+//     },
+// };
+
+// function init() {
+//     menuLoading.style.display = "none";
+//     startButton.style.display = "block";
+
+//     Object.values(models).forEach((model, index) => {
+//         const clonedScene = clone(model.gltf.scene);
+//         const root = new THREE.Object3D();
+//         root.add(clonedScene);
+//         scene.add(root);
+//         root.position.x = (index -3) * 3;
+//     });
+// }
+
+// const manager = new THREE.LoadingManager();
+// manager.onLoad = init;
+
+
+// const gltfLoader = new GLTFLoader(manager);
+// for (const model of Object.values(models)) {
+//     gltfLoader.load(model.url, (gltf) => {
+//         model.gltf = gltf;
+//     });
+// }
+
+const objLoader = new THREE.ObjectLoader();
+objLoader.setPath('/models/');
+objLoader.load('tinker.obj', (object) => {
+    scene.add(object);
+})
 
 const geometry = new THREE.SphereGeometry(300, 30, 30);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
@@ -44,37 +82,8 @@ onkeydown = onkeyup = function (e) {
     usedKeys[e.keyCode] = e.type == 'keydown';
 }
 
-function loop() {
-    if(controls.isLocked ){
-        let forward = 10;
-        let sideways = 7;
-        if (usedKeys[16]) {
-            forward *= 2;
-            sideways *= 2;
-        }
-        // mesh.rotation.y += 0.01;
-        if (usedKeys[87]) {
-            camera.position.z -= forward;
-        }
-        if (usedKeys[83]) {
-            camera.position.z += forward;
-        }
-        if (usedKeys[65]) {
-            camera.position.x -= sideways;
-        }
-        if (usedKeys[68]) {
-            camera.position.x += sideways;
-        }
-    }
 
-    mesh.rotation.y += 0.01;
 
-    renderer.render(scene, camera);
-    requestAnimationFrame(function () { loop(); });
-}
-
-const startButton = document.getElementById("start_button");
-const menu = document.getElementById("menu");
 const controls = new PointerLockControls(camera, document.body);
 
 startButton.onclick = () => {
@@ -103,7 +112,35 @@ window.addEventListener('resize', function (event) {
     camera.updateProjectionMatrix();
 });
 
+function loop() {
+    if (controls.isLocked) {
+        let forward = 10;
+        let sideways = 7;
+        if (usedKeys[16]) {
+            forward *= 2;
+            sideways *= 2;
+        }
+        // mesh.rotation.y += 0.01;
+        if (usedKeys[87]) {
+            controls.moveForward(forward);
+        }
+        if (usedKeys[83]) {
+            controls.moveForward(-forward);
+        }
+        if (usedKeys[65]) {
+            controls.moveRight(-sideways)
+            // camera.position.x -= sideways;
+        }
+        if (usedKeys[68]) {
+            controls.moveRight(sideways)
+            // camera.position.x += sideways;
+        }
+    }
+
+    mesh.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+    requestAnimationFrame(function () { loop(); });
+}
+
 loop();
-
-
-
